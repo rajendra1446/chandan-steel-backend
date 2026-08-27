8c30ed5a06cffce882c39f811444bf864a0eb164
RAJENDRA123468 <rc7874324@gmail.com>
2026-08-27 21:46:17 +0530

    connection string

--- src/config/db.js
+++ src/config/db.js
@@ -6,16 +6,11 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 5000
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
-
 pool.on("connect", () => {
     console.log("PostgreSQL connected");
 });
